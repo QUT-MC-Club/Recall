19df9e35295a10ef1c461c0835b000afe2e016e0
4dTailor <91608035+4dTailor@users.noreply.github.com>
2025-02-24 12:31:24 +1000

    Added tooltips to return apples and potion

--- src/main/java/me/skyquiz/recall/item/ReturnApple.java
+++ src/main/java/me/skyquiz/recall/item/ReturnApple.java
@@ -1,5 +1,10 @@
 package me.skyquiz.recall.item;
 
+import com.jamieswhiteshirt.rtree3i.Entry;
+import com.jamieswhiteshirt.rtree3i.Selection;
+import draylar.goml.api.Claim;
+import draylar.goml.api.ClaimBox;
+import draylar.goml.api.ClaimUtils;
 import eu.pb4.polymer.core.api.item.SimplePolymerItem;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import me.skyquiz.recall.Recall;
@@ -8,12 +13,18 @@ import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.*;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import xyz.nucleoid.packettweaker.PacketContext;
+
+import java.util.List;
 
 public class ReturnApple extends SimplePolymerItem {
     private static final FoodComponent RETURN_APPLE_COMPONENT = new FoodComponent.Builder()
@@ -50,5 +61,10 @@ public class ReturnApple extends SimplePolymerItem {
             player.addStatusEffect(new StatusEffectInstance(Recall.RECALL, Recall.TIME_TO_TP_TICKS, 0));
         }
         return super.finishUsing(stack, world, user);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("item.recall.return_apple.tooltip").formatted(Formatting.DARK_GRAY));
     }
 }
