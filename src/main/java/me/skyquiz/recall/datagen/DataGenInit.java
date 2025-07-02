9fe6cb4306547fa9bf7e9f4b5e2c8658221c7ea8
4dTailor <91608035+4dTailor@users.noreply.github.com>
2025-07-03 00:29:31 +1000

    Added recipes

---
+++ src/main/java/me/skyquiz/recall/datagen/DataGenInit.java
@@ -0,0 +1,14 @@
+package me.skyquiz.recall.datagen;
+
+import me.skyquiz.recall.RecallRecipeProvider;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenInit implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        var pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(RecallRecipeProvider::new);
+    }
+}
